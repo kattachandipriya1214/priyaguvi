87e563eb4ab53a3b094d6d88f43a69f721260b59
kattachandipriya1214 <41769784+kattachandipriya1214@users.noreply.github.com>
2018-07-27 16:13:48 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,7 @@
 str=raw_input()
 if str in('a' 'e' 'i' 'o' 'u' 'A' 'E' 'I' 'O' 'U'):
-	print("vowel")
+	print("Vowel")
+elif((str>='a' and str<='z') or (str>='A' and str='Z')):
+	print("Invalid")
 else:
-	print("consonant")
+	print("Consonant")
